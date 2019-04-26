b2e5cc339aa2c691ae21379cd351576504990854
Dominique Dixon <domdrop92@hotmail.com>
2019-04-25 18:41:34 -0700

    did review for hw

---
+++ intro-to-js/js/review-data-types-1.js
@@ -0,0 +1,93 @@
+// integer variables
+
+var a = 10;
+console.log(a);
+var b = 20;
+console.log(b);
+var c = 30;
+console.log(c);
+var d = 15;
+console.log(d);
+var e = 25;
+console.log(e);
+
+// string variables
+
+var str1 = "Hi";
+console.log(str1);
+var str2 = "My name is";
+console.log(str2);
+var str3 = "What?"
+console.log(str3);
+var str4 = "My name is";
+console.log(str4);
+var str5 = "Who?";
+console.log(str5);
+
+// boolean variables
+
+var a = true;
+var b = false;
+
+console.log(24 == 24);
+console.log(65 ===65);
+
+// array variables
+
+var arr1 = [ "Hi" , "My" , "Name" , "Is" ];
+console.log(arr1);
+
+var arr2 = ["what"]
+console.log(arr2);
+
+var arr3 = ["My" , "Name" , "Is" ];
+console.log(arr3);
+
+var arr4 = [ "who" ];
+console.log(arr4);
+
+var arr5 = [ "My" , "Name" , "Is" , "Dominique" ];
+console.log(arr5);
+
+
+// object variables
+
+var date = {
+    month: 'April',
+    day: 'Wednesday',
+    year: '2019',
+}
+console.log(date);
+
+var car = {
+    year: '2010',
+    make: 'chevy',
+    model: 'impala',
+    color: 'black',
+}
+console.log(car);
+
+var school = {
+    name: 'SHS',
+    year: '2010',
+    mascot: 'cowboy',
+}
+console.log(school);
+
+var food = {
+    breakfast: 'eggs',
+    lunch: 'salad',
+    dinner: 'chicken',
+    desert: 'brownies',
+    snacks: 'fruit',
+}
+console.log(food);
+
+var classes = {
+    first: 'math',
+    second: 'english',
+    third: 'history',
+    fourth: 'science',
+}
+console.log(classes);
+
