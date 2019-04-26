226eaa237a12d0243d0e93dd3de7133d53f88de3
Dominique Dixon <domdrop92@hotmail.com>
2019-04-25 18:53:34 -0700

    reviewed numbers

---
+++ intro-to-js/js/review-number.js
@@ -0,0 +1,13 @@
+var num1 = 145;
+var num2 = 458;
+
+var sum = num1 + num2;
+console.log(sum);
+var diff = num1 - num2;
+console.log(diff);
+var mult = num1 * num2;
+console.log(mult);
+var div = num1 / num2;
+console.log(div);
+var mod = num1 % num2;
+console.log(mod);
